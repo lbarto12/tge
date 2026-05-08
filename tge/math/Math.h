9b02a4a262e958d500d6ccbb70af5ffa389bda82
Liam Barrack <lbarto12@hotmail.com>
2026-05-08 17:27:41 -0400

    add docstring

--- tge/math/Math.h
+++ tge/math/Math.h
@@ -7,6 +7,13 @@ class Math {
 public:
     Math() = delete;
 
+    /**
+     * Clamp a value between two values, inclusive.
+     *
+     * @param lo the lower bound
+     * @param hi the upper bound
+     * @return clamped value
+     */
     template <typename T> static T Clamp(T lo, T hi, T val) { return std::max(lo, std::min(hi, val)); };
 };
 } // namespace tge
